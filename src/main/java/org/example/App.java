570de63187498e0ad3f9f90105f04cf03c01f916
admin <administrator@jits-3-28.jits.ipa.dc>
2022-07-03 16:03:25 +0600

    add ManyToMany realtionship

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,8 +1,6 @@
 package org.example;
 
-import org.example.model.Item;
-import org.example.model.Passport;
-import org.example.model.Person;
+import org.example.model.*;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -20,30 +18,52 @@ public class App
     public static void main( String[] args )
     {
         //Подключаем конфигурацию из файла hibernate.properties (hibernate сам получает из него конфигурацию под капотом).
-       Configuration configuration = new Configuration().addAnnotatedClass(Person.class).addAnnotatedClass(Passport.class); //Этой конфигурации мы должны передать класс, который является нашей сущностью (класс который помечен @Entity)
+       Configuration configuration = new Configuration().addAnnotatedClass(Actor.class).addAnnotatedClass(Movie.class); //Этой конфигурации мы должны передать класс, который является нашей сущностью (класс который помечен @Entity)
         //Теперь Hibernate будет видеть этот класс Person и будет понимать что в базе есть таблица Person которая соответствует классу Person.
 
         //Получаем SessionFactory чтобы из него получить сессию для работы с Hibernate
         SessionFactory sessionFactory = configuration.buildSessionFactory();
-        Session session = sessionFactory.getCurrentSession();
 
-        try {
+
+        //try with resources
+        try (sessionFactory){
+            Session session = sessionFactory.getCurrentSession();
             session.beginTransaction();
 
-//            Person person = new Person("Test person", 50);
-//            Passport passport = new Passport(123456);
+//            Movie movie = new Movie("Pulp fiction", 1994);
+//            Actor actor1 = new Actor("Harvey Keitel", 81);
+//            Actor actor2 = new Actor("Samuel L. Jackson", 72);
 //
-//            person.setPassport(passport);
+//            movie.setActors(new ArrayList<>(List.of(actor1, actor2)));
 //
-//            session.save(person);
+//            actor1.setMovies(new ArrayList<>(Collections.singletonList(movie)));
+//            actor2.setMovies(new ArrayList<>(Collections.singletonList(movie)));
+//
+//            session.save(movie);
+//            session.save(actor1);
+//            session.save(actor2);
 
-            Person person = session.get(Person.class, 1);
-            session.remove(person);
+//            Movie movie = session.get(Movie.class, 1);
+//            System.out.println(movie.getActors());
+
+//            Movie movie = new Movie("Reservoir Dogs", 1992);
+//            Actor actor = session.get(Actor.class, 1);
+//
+//            movie.setActors(new ArrayList<>(Collections.singletonList(actor)));
+//
+//            actor.getMovies().add(movie);
+//
+//            session.save(movie);
+
+            Actor actor = session.get(Actor.class, 2);
+            System.out.println(actor.getMovies());
+
+            Movie movieToRemove = actor.getMovies().get(0);
+
+            actor.getMovies().remove(0);
+            movieToRemove.getActors().remove(actor);
 
             session.getTransaction().commit();
-        }
-        finally {
-            sessionFactory.close();
         }
     }
 }
